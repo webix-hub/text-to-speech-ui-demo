8c6916bdd813e300b4f66864044ac2c98e4f0b08
olistopad <olistopad@xbsoftware.com>
2025-11-25 16:25:39 +0300

    [fix] demo audio URL

--- data.js
+++ data.js
@@ -6,7 +6,7 @@ export const historyCollection = new webix.DataCollection({
       preview: "This UI is built with Webix.",
       voice: "nova",
       audioUrl:
-        "https://docs.webix.com/filemanager-backend/direct?id=%2FMusic%2Fbensound-betterdays.mp3",
+        "https://files.webix.com/30d/1fd73c28c861e46c30b66b87e20e1e36/bensound-betterdays.mp3",
     },
   ],
 });
